918e2e63f20dd7d3ec0137a09b9de8259f3e03d1
ShlomiShakoy <103436003+ShlomiShakoy@users.noreply.github.com>
2022-08-08 17:30:17 +0300

    Add files via upload

---
+++ Obstacle Game/Obstacle Game/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace Obstacle_Game
+{
+    partial class Form1:Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.CSharp = new System.Windows.Forms.PictureBox();
+            this.Amit = new System.Windows.Forms.PictureBox();
+            this.Shlomi = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.CSharp)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Amit)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Shlomi)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtScore
+            // 
+            this.txtScore.AutoSize = true;
+            this.txtScore.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScore.Location = new System.Drawing.Point(13, 13);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(102, 31);
+            this.txtScore.TabIndex = 6;
+            this.txtScore.Text = "Score:0";
+            this.txtScore.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.MainGameTimerEvent);
+            // 
+            // CSharp
+            // 
+            this.CSharp.Image = global::Obstacle_Game.Properties.Resources.c__logo;
+            this.CSharp.Location = new System.Drawing.Point(534, 400);
+            this.CSharp.Name = "CSharp";
+            this.CSharp.Size = new System.Drawing.Size(50, 50);
+            this.CSharp.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.CSharp.TabIndex = 10;
+            this.CSharp.TabStop = false;
+            this.CSharp.Tag = "obstacle";
+            this.CSharp.Click += new System.EventHandler(this.CSharp_Click);
+            // 
+            // Amit
+            // 
+            this.Amit.Image = global::Obstacle_Game.Properties.Resources.fatma;
+            this.Amit.Location = new System.Drawing.Point(386, 400);
+            this.Amit.Name = "Amit";
+            this.Amit.Size = new System.Drawing.Size(67, 55);
+            this.Amit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Amit.TabIndex = 8;
+            this.Amit.TabStop = false;
+            this.Amit.Tag = "obstacle";
+            this.Amit.Click += new System.EventHandler(this.Amit_Click);
+            // 
+            // Shlomi
+            // 
+            this.Shlomi.Image = global::Obstacle_Game.Properties.Resources.shlomi;
+            this.Shlomi.Location = new System.Drawing.Point(40, 395);
+            this.Shlomi.Name = "Shlomi";
+            this.Shlomi.Size = new System.Drawing.Size(44, 60);
+            this.Shlomi.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Shlomi.TabIndex = 7;
+            this.Shlomi.TabStop = false;
+            this.Shlomi.Tag = "ShlomiTag";
+            this.Shlomi.Click += new System.EventHandler(this.shlomi2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.pictureBox1.Location = new System.Drawing.Point(-10, 456);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(2065, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(1279, 458);
+            this.Controls.Add(this.Shlomi);
+            this.Controls.Add(this.Amit);
+            this.Controls.Add(this.CSharp);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtScore);
+            this.Name = "Form1";
+            this.Tag = "CppTag";
+            this.Text = "Obstacle game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.CSharp)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Amit)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Shlomi)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.Timer gameTimer;
+        private PictureBox Shlomi;
+        private PictureBox Amit;
+        private PictureBox CSharp;
+    }
+}
+
---
+++ Obstacle Game/Obstacle Game/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Obstacle_Game
+{
+    public partial class Form1 : Form
+    {
+        bool jumping = false;
+        int jumpSpeed;
+        int force = 12;
+        int score = 0;
+        int obstacleSpeed = 10;
+        Random rand = new Random();
+        int position;
+        bool isGameOver = false;
+        public Form1()
+        {
+            InitializeComponent();
+            gameReset();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space && jumping == false)//if the space keyboard is pressed 
+            {
+                jumping = true;
+            }
+        }
+
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            if (jumping == true)
+            {
+                jumping = false;
+            }
+            if(e.KeyCode==Keys.R &&isGameOver==true)
+            {
+                gameReset();
+            }
+        }
+        private void gameReset()
+        {
+            force = 12;
+            jumpSpeed = 0;
+            jumping = false;
+            score = 0;
+            obstacleSpeed = 10;
+            txtScore.Text = "Score " + score;
+            Shlomi.Image = Properties.Resources.shlomi;
+            isGameOver = false;
+            Shlomi.Top = 367;
+            foreach(Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "obstacle")//if the object is photo and also obstacle.
+                                                                   //the Control is connected to Form and it displays information of the user
+                {
+                    position = this.ClientSize.Width + rand.Next(500, 800) + (x.Width * 10);
+
+                    x.Left = position;
+                }
+            }
+            gameTimer.Start();
+        }
+
+        private void pictureBox2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void shlomi2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Amit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CSharp_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainGameTimerEvent(object sender, EventArgs e)
+        {
+            Shlomi.Top += jumpSpeed;
+            txtScore.Text = "Score: " + score;
+            if(jumping == true && force<0)
+            {
+                jumping = false;
+            }
+            if(jumping == true)
+            {
+                jumpSpeed = -12;
+                force -= 1;
+            }
+            else 
+            {
+                jumpSpeed = 12;
+            }
+            if(Shlomi.Top>366&&jumping==false)
+            {
+                force = 12;
+                Shlomi.Top = 367;
+                jumpSpeed = 0;
+            }
+            foreach(Control x in this.Controls)
+            {
+                if(x is PictureBox &&(string)x.Tag=="obstacle")
+                {
+                    x.Left -= obstacleSpeed;
+                    if(x.Left<-100)
+                    {
+                        x.Left = this.ClientSize.Width + rand.Next(200, 500) + (x.Width * 15);
+                        score++;
+                    }
+                    if(Shlomi.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        gameTimer.Stop();
+                        Shlomi.Image = Properties.Resources.shlomi;//when shlomi is dead
+                        txtScore.Text += "  press R to restart the game!";
+                        isGameOver = true;
+                    }
+                }
+            }
+            if(score>10)
+            {
+                obstacleSpeed = 15;
+            }
+        }
+    }
+}
